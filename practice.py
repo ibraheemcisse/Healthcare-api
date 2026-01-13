079683dc72b74de2797e566c00a04a3bd45db4dc
Dell Linux <dell-linux@ibrahimcisse@ioc-labs.com>
2026-01-13 14:16:48 +0800

    feat: add patient filtering by age condition

--- practice.py
+++ practice.py
@@ -3,9 +3,16 @@ patients = []
 patients.append({"name": "John", "age": 30, "condition": "flu"})
 patients.append({"name": "Jake", "age": 25, "condition": "checkup"})
 patients.append({"name": "Suzy", "age": 45, "condition": "diabetes"})
+patients.append({"name": "ibrahim", "age": 28, "condition": "allergy"})
 
 print(f"Total patients: {len(patients)}")
 
 for patient in patients:
     print(f"{patient['name']}, age {patient['age']}, visiting for: {patient['condition']}")
 
+#filter - only patients older than 30 
+
+print("\n--- Patients over 30 ---")
+for patient in patients:
+    if patient['age'] > 30:
+        print(f"{patient['name']}, is {patient['age']} years old.")
